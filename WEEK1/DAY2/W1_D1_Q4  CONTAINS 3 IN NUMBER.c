fb791a813dedd5e506c30613121e57290f638a47
MadhuDadanje <108974027+MadhuDadanje@users.noreply.github.com>
2022-07-13 15:39:54 +0530

    Update W1_D1_Q4  CONTAINS 3 IN NUMBER.c

--- WEEK1/DAY2/W1_D1_Q4  CONTAINS 3 IN NUMBER.c
+++ WEEK1/DAY2/W1_D1_Q4  CONTAINS 3 IN NUMBER.c
@@ -4,29 +4,50 @@
 3.check whether digit contain 3 or nor if exist print that no...
 4.other wise increment value
 */
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+/*
+1.read n1 and n2 
+2.check given range 
+3.check whether digit contain 3 or nor if exist print that no...
+4.other wise increment value
+*/
 #include <stdio.h>
 int main()
 {
-int n1,n2,r=0,k=0,s,t=0;
-scanf("%d %d",&n1,&n2);
+int n1,n2,r=0,k=0,s,t=0,cnt=0;
+scanf("%d",&n1);
 l:
-if(n1<=n2)
-{k=n1;
-s=n1;
+if(n1>0)
+{;
+scanf("%d",&k);
+s=k;
+n1--;
 whl:
 if(s!=0)
 {
     r=s%10;
-    if(r==3){
-     printf(" %d",k);  
+    if(r==3){cnt++;
+    // printf(" lucky no =%d\n ",k);  
         goto out;
     }s/=10; 
     goto whl;
 }
 out:
-n1++;
 goto l;
+ 
 }
+printf(" lucky count  =%d\n ",cnt);  
 }
+
+
+
+
+
 
 
